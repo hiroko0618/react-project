9b44dbb32b0070a1aa21a06fd8160db518aad6da
ZoungranaHiroko <hiroko@seamaple.co.jp>
2025-10-30 12:42:33 +0900

    edit parameter

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,23 +1,37 @@
 import { EStatResponseType } from '../types/estatParams';
 import { getMessage } from '../messages/message';
 
-export async function getEstatData(): Promise<EStatResponseType> {
+export const getEstatData = async (): Promise<EStatResponseType> => {
+
   const baseUrl = 'https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData';
-  const parameter = 'cdArea=08000%2C09000%2C10000%2C11000%2C12000%2C13000%2C14000%2C19000&cdCat01=A1101&lang=J&statsDataId=0000010101&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0';
-  const appId = process.env.REACT_APP_ESTAT_API_APPID;
+  const appId = process.env.REACT_APP_ESTAT_API_APPID!;
+  const parameter = process.env.REACT_APP_ESTAT_API_PARAMETER!;
   const url = `${baseUrl}?appId=${appId}&${parameter}`;
   const proxyUrl = `https://corsproxy.io/?${url}`;
-
+  
   try {
     const response = await fetch(proxyUrl, {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
     });
+
+    if (!response.ok) {
+      throw new Error(`${getMessage.getDatafailed}, ${response.status} ${response.statusText}`);
+    }
+
     const json = await response.json();
-    
+
+    if (!isValidEstatResponse(json)) {
+      throw new Error(getMessage.getDatafailed);
+    }
+
     return json as EStatResponseType;
   } catch (e: any) {
     console.error(getMessage.showDetail, e);
     throw e;
   }
+}
+
+const isValidEstatResponse = (data: any): boolean => {
+  return data && typeof data === 'object' && 'GET_STATS_DATA' in data;
 }
